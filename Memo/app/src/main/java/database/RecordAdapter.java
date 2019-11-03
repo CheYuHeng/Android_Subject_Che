e851c4ff93f31e9379590a803db9ff3fc84d7f88
your name <your email>
2019-11-03 22:11:29 +0800

    实验二，记事本

--- Memo/app/src/main/java/database/RecordAdapter.java
+++ Memo/app/src/main/java/database/RecordAdapter.java
@@ -1,5 +1,42 @@
 package database;
 
-public class RecordAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.memo.R;
+
+import java.util.ArrayList;
+import bean.Record;
+
+public class RecordAdapter extends BaseAdapter {
+    LayoutInflater inflater;
+    ArrayList<Record> arrlist;
+    Context context;
+
+    public RecordAdapter(Context context,ArrayList<Record> arrlist){
+        this.context = context;
+        this.arrlist = arrlist;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return arrlist.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrlist.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
 
 }
--- Memo/app/src/main/java/database/RecordDataBase.java
+++ Memo/app/src/main/java/database/RecordDataBase.java
@@ -1,5 +1,80 @@
 package database;
 
-public class RecordDataBase {
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+
+import bean.Record;
+
+public class RecordDataBase {
+    Context context;
+    RecordOpenHelper rdOpenHelper;
+    SQLiteDatabase rddatabase;
+
+    public RecordDataBase(Context context){
+        this.context = context;
+        rdOpenHelper = new RecordOpenHelper(context);
+    }
+
+    public ArrayList<Record> getArray(){
+        ArrayList<Record> array = new ArrayList<Record>();
+        ArrayList<Record> array_1 = new ArrayList<Record>();
+
+        rddatabase = rdOpenHelper.getWritableDatabase();
+
+        Cursor cursor = rddatabase.rawQuery("select num,title,times from myrecord",null);
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            int id = cursor.getInt(cursor.getColumnIndex("ids"));
+            String title = cursor.getString(cursor.getColumnIndex("title"));
+            String times = cursor.getString(cursor.getColumnIndex("times"));
+            Record record = new Record(id, title, times);
+            array.add(record);
+            cursor.moveToNext();
+        }
+        rddatabase.close();
+        for (int i = array.size(); i > 0; i--) {
+            array_1.add(array.get(i-1));
+        }
+        return array_1;
+    }
+
+    public Record getEditRecord(int num){
+        rddatabase = rdOpenHelper.getWritableDatabase();
+        Cursor cursor = rddatabase.rawQuery("select title,content from myrecord where num='"+ num +"'" , null);
+        cursor.moveToFirst();
+        String title=cursor.getString(cursor.getColumnIndex("title"));
+        String content=cursor.getString(cursor.getColumnIndex("content"));
+        Record record = new Record(title,content);
+        rddatabase.close();
+        return record;
+    }
+
+    public void toUpdate(Record record){           //修改表中数据
+        rddatabase = rdOpenHelper.getWritableDatabase();
+        rddatabase.execSQL(
+                "update myrecord set title = '" + record.getTitle()+
+                        "',times = '" + record.getTimes() +
+                        "',content = '" + record.getContent() +
+                        "' where ids = '" + record.getNum()+"'");
+        rddatabase.close();
+    }
+
+    public void toInsert(Record record){           //在表中插入新建的便签的数据
+        rddatabase = rdOpenHelper.getWritableDatabase();
+        rddatabase.execSQL("insert into myrecord(title,content,times)values('"
+                + record.getTitle()+"','"
+                + record.getContent()+"','"
+                + record.getTimes()
+                + "')");
+        rddatabase.close();
+    }
+
+    public void toDelete(int num){            //在表中删除数据
+        rddatabase  = rdOpenHelper.getWritableDatabase();
+        rddatabase.execSQL("delete from myrecord where num =" + num +"");
+        rddatabase.close();
+    }
 }
