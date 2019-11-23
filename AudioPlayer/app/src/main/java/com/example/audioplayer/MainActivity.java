420bf22aa175a0f879f0b4c53cae5c443500ad96
your name <your email>
2019-11-23 19:54:48 +0800

    实验三，音乐播放器

--- AudioPlayer/app/src/main/java/com/example/audioplayer/MainActivity.java
+++ AudioPlayer/app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -16,6 +17,7 @@ import android.os.Message;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -27,21 +29,83 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import Adapter.MusicAdapter;
+import Util.MusicUtil;
 import bean.Music;
 
 public class MainActivity extends AppCompatActivity{
 
-    ListView myMusic;
+    private static final String TAG = "MainActivity";
     List<Music> lists;
+    MusicAdapter adapter;
+    ListView listView;
     MediaPlayer mediaPlayer;
+    SeekBar seek;
+    Timer timer = new Timer();
     int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seek = findViewById(R.id.seek);
+        listView = findViewById(R.id.listView);
+
+        seek.setProgress(0);
         mediaPlayer = new MediaPlayer();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
+
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b == true){
+                    mediaPlayer.seekTo(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        lists = new MusicUtil().getMusic(this);
+        Log.i(TAG, "" + lists);
+        adapter = new MusicAdapter(lists, this);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                index = i;
+                seek.setProgress(0);
+                mediaPlayer.reset();
+                try {
+                    seek.setMax(Integer.parseInt(lists.get(i).getDuration()));
+                    mediaPlayer.setDataSource(lists.get(i).getData());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                seek.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,1000);
 
     }
 
